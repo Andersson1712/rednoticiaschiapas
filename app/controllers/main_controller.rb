f0657a8f641a1b1dd13139b8c5036686c02143d8
Andersson <andersson17_122013@outlook.com>
2017-08-16 21:55:03 -0500

    responsive

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,7 +2,7 @@ class MainController < ApplicationController
   def index
  @comments = Comment.all
  @comments = Comment.new
- @articles = Article.all
+ @articles = Article.paginate(page: params[:page],per_page:6).ultimos
  @article = Article.new
   end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ validates :title, presence: true, length:{maximum:50}
 validates :description, presence: true, length:{minimum:20}
 before_save :set_visit_count
 
-has_attached_file :cover, styles: { medium: "1280x720>", thumb: "900x300>" }, default_url: "/images/:style/missing.png"
+has_attached_file :cover, styles: { medium: "1280x720>", thumb: "800x600>" }, default_url: "/images/:style/missing.png"
 validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
 
 scope :ultimos, ->{order("created_at DESC").limit(6)}
